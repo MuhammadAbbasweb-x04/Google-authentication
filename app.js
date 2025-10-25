37cc3d4774e68a0b6de172bdae9e631ab74c5bdb
MuhammadAbbasweb-x04 <mabbasmshahi2003@gmail.com>
2025-10-25 11:30:57 -0700

    done

---
+++ app.js
@@ -0,0 +1,16 @@
+import {signInWithPopup} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { auth ,provider} from "./authconfig.js";
+let btn = document.querySelector(".google-btn");
+btn.addEventListener("click" , ()=>{
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const user = result.user;
+    console.log(user);
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
+    
+  });
+})
+
