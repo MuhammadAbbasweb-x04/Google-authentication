db06ea4957e9ebe4e163b070bbbd92c2bd91fc3e
MuhammadAbbasweb-x04 <mabbasmshahi2003@gmail.com>
2025-11-01 23:06:17 -0700

    100%  done

--- logout.js
+++ logout.js
@@ -1,7 +1,34 @@
+import {signOut,
+onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { auth } from "./authconfig.js";
+
 const logoutbtn = document.querySelector(".logout-btn");
-
-logoutbtn.addEventListener("click" , ()=>{
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    console.log(`User uid : ${uid}`);
+  } else {
 window.location = "index.html";
+  }
+});
 
 
-})
+logoutbtn.addEventListener("click" , () =>{
+signOut(auth)
+.then(() => {
+window.location = "index.html"
+}).catch((error) => {
+console.log(error, "error");
+}); 
+} )
+
+
+
+
+
+
+
+
+
+
